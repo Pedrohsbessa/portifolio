e71455333954def97ee1fe304d218b91bf836fc3
Pedro Henrique Bessa <99356444+Pedrohsbessa@users.noreply.github.com>
2023-10-17 11:57:12 -0300

    footer test 3

--- portifolio/src/scenes/Footer.jsx
+++ portifolio/src/scenes/Footer.jsx
@@ -3,7 +3,7 @@ import SocialMediaIcons from "../components/SocialMediaIcons";
 const Footer = () => {
   return (
     <footer className=" bg-red pt-10 clear-both absolute h-24 bottom-0">
-      <div className="w-10/12 mx-auto h-24 bottom-0  ">
+      <div className="w-10/12 mx-auto h-24 bottom-0 relative ">
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center ">
           <p className="font-playfair font-semibold text-2xl text-yellow">
