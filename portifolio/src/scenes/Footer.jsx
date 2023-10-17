e2254fae0bc177bc64f7230b63cc2da351175511
Pedro Henrique Bessa <99356444+Pedrohsbessa@users.noreply.github.com>
2023-10-17 11:52:28 -0300

    footer test 1

--- portifolio/src/scenes/Footer.jsx
+++ portifolio/src/scenes/Footer.jsx
@@ -2,15 +2,15 @@ import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
   return (
-    <footer className=" bg-red pt-10 clear-both relative mt-[-200px] h-24 bottom-0">
-      <div className="w-10/12 mx-auto h-24">
+    <footer className=" bg-red pt-10 clear-both absolute h-24 bottom-0">
+      <div className="w-10/12 mx-auto h-24 ">
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center ">
           <p className="font-playfair font-semibold text-2xl text-yellow">
-            JANE ESPER
+            Your Name
           </p>
           <p className="font-playfair text-md text-yellow">
-            ©2022 ESPER. All Rights Reserved.
+            ©2022 Your. All Rights Reserved.
           </p>
         </div>
       </div>
